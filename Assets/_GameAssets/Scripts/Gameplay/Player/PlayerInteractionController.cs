35c12ff89542122ecaa5bfb13bb9da5ade084904
qarimbas <71667807+qarimbas@users.noreply.github.com>
2026-03-26 16:57:16 +0300

    interface for collectible

--- Assets/_GameAssets/Scripts/Gameplay/Player/PlayerInteractionController.cs
+++ Assets/_GameAssets/Scripts/Gameplay/Player/PlayerInteractionController.cs
@@ -4,22 +4,9 @@ public class PlayerInteractionController : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag(Consts.WheatTypes.GOLD_WHEAT))
+        if (other.gameObject.TryGetComponent<ICollectible>(out var collectible))
         {
-            //Debug.Log("Player collected GOLD wheat!");
-            other.gameObject?.GetComponent<GoldWheatCollectible>().Collect();
-        }
-
-        if (other.CompareTag(Consts.WheatTypes.HOLY_WHEAT))
-        {
-            //Debug.Log("Player collected HOLY wheat!");
-            other.gameObject?.GetComponent<HolyWheatCollectible>().Collect();
-        }
-
-        if (other.CompareTag(Consts.WheatTypes.ROTTEN_WHEAT))
-        {
-            //Debug.Log("Player collected ROTTEN wheat!");
-            other.gameObject?.GetComponent<RottenWheatCollectible>().Collect();
+            collectible.Collect();
         }
     }
 }
