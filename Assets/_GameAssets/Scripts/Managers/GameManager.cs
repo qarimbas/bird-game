ccb7eb4978616f79a75850a8ac8c4f6bfbeeed72
qarimbas <71667807+qarimbas@users.noreply.github.com>
2026-03-28 12:08:52 +0300

    EggUI

--- Assets/_GameAssets/Scripts/Managers/GameManager.cs
+++ Assets/_GameAssets/Scripts/Managers/GameManager.cs
@@ -4,6 +4,13 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
 
+    [Header("References")]
+
+    [SerializeField] private EggCounterUI _eggCounterUI;
+
+
+    [Header("Egg Collection Settings")]
+
     [SerializeField] private int _maxEggCount = 5;
 
     private int _currentEggCount;
@@ -16,10 +23,13 @@ public class GameManager : MonoBehaviour
     public void OnEggCollected()
     {
         _currentEggCount++;
+        _eggCounterUI.SetEggCounterText(_currentEggCount, _maxEggCount);
+
         if (_currentEggCount == _maxEggCount)
         {
             // Implement win 
             Debug.Log("All eggs collected! You win!");
+            _eggCounterUI.SetEggCompleted();
         }
         Debug.Log($"Egg collected! Current count: {_currentEggCount}/{_maxEggCount}");
     }
